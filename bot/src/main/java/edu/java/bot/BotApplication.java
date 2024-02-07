e60d5011bfef8d9bfebd55f0b8c5d37d87a11815
skiftare <b.kirill.ig@gmail.com>
2024-02-07 19:30:54 +0500

    Разобрался как прятать токен, ура!

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -1,14 +1,53 @@
 package edu.java.bot;
 
+
 import edu.java.bot.configuration.ApplicationConfig;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class BotApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BotApplication.class, args);
+        //SpringApplication.run(BotApplication.class, args);
+
+        SpringApplication application = new SpringApplication(BotApplication.class);
+        application.addInitializers(new EnvConfig());
+        ApplicationContext context = application.run(args);
+
+        // Получаем экземпляр класса TelegramTokenPrinter из контекста
+        TelegramTokenPrinter tokenPrinter = context.getBean(TelegramTokenPrinter.class);
+
+        // Выводим значение токена на консоль
+        tokenPrinter.printToken();
+
+
     }
+
+    static class EnvConfig implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
+            Dotenv dotenv = Dotenv.configure().load();
+            dotenv.entries().forEach(entry -> {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                applicationContext.getEnvironment().getSystemProperties().put(key, value);
+            });
+        }
+    }
+
+
 }
+
+
